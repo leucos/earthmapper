d89ac455307e3a3c724a430f671c8a37b8e68a54
Michel Blanc <mb@mbnet.fr>
2012-12-10 16:37:17 +0100

    Adds backend config loading

--- app.rb
+++ app.rb
@@ -11,6 +11,12 @@ Ramaze.options.roots = [__DIR__]
 # Get setings
 require __DIR__('config/settings.rb')
 
+# Get backends settings
+EarthMapper.options.backends.each do |b|
+  Ramaze::Log.info("Loading settings for backend #{b[:name]} (#{b[:description]})")
+  require File.join(__DIR__('config'), b[:name])
+end
+
 # Initialize controllers and modules
 #require __DIR__('controller/init')
 require __DIR__('modules/init')
